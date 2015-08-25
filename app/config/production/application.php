02a0485486037cdc18b2418c9e23b2bcae3bb61e
Khánh Trần <rualatngua@users.noreply.github.com>
2015-08-25 20:59:40 +0700

    Use environment variables instead of hardcode
    DB_HOST
    DB_NAME
    DB_USER
    DB_PASS

--- app/config/production/application.php
+++ app/config/production/application.php
@@ -3,10 +3,10 @@
 return array(
     'database' => array(
         'adapter' => 'Mysql',
-        'host' => 'localhost',
-        'username' => '',
-        'password' => '',
-        'dbname' => '',
+        'host' => getenv('DB_HOST'),
+        'username' => getenv('DB_USER'),
+        'password' => getenv('DB_PASS'),
+        'dbname' => getenv('DB_NAME'),
         'charset' => 'utf8',
     ),
     'cache' => 'file', // file, memcache
